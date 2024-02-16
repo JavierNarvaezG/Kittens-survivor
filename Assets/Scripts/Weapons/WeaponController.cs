16e20cbf7301b8a7230a2d73f5bc122ddfdae8eb
JavierNarvaezG <153122918+JavierNarvaezG@users.noreply.github.com>
2024-02-16 17:49:35 -0500

    se implementa mecanica de espawn del arma y sus componentes

--- Assets/Scripts/Weapons/WeaponController.cs
+++ Assets/Scripts/Weapons/WeaponController.cs
@@ -2,19 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//<Summary>
+// Base script for all weapon controllers
+//</Summary>
+
 public class WeaponController : MonoBehaviour
 {
     [Header("weapon Stats")]
     public GameObject prefabs;
-    // Start is called before the first frame update
-    void Start()
+    public float damage;
+    public float speed;
+    public float cooldownDuration;
+    public float currentCooldown;
+    public float pierce;
+
+    // Start is called before the first frame update   
+    //The protected keyword is another visibility state, similar to public, but it only allows accessibility to the child class from outside
+    //the virtual keyword allows override an method, property, indexer or event declaration on any child class
+    //i use this metod be cause i want to try using an "security" metod and saw the limits using this accessibility states.
+    protected virtual void Start()
     {
-        
+        currentCooldown = cooldownDuration; //At the start set the current cooldown to be the cooldown duration
     }
 
     // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
-        
+        currentCooldown -= Time.deltaTime;
+        if (currentCooldown < 0f) //Once the cooldown becomes 0, attack
+        {
+            Attack();
+        }
+    }
+
+    protected virtual void Attack() 
+    {
+        currentCooldown = cooldownDuration;
     }
 }
---
+++ Assets/Scripts/Weapons/YarnController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YarnController : WeaponController
+{   
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    //there we are overriding the attack method on weapon controller and the same thing with Start()
+    protected override void Attack() 
+    {
+        base.Attack();
+        GameObject spawnedYarn = Instantiate(prefabs);
+        spawnedYarn.transform.position = transform.position; //Assign the position to be the same as this object which is parented to the player
+    }
+}
